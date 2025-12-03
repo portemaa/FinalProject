7935bb5fba752b91af556af3e2c1d20d61f390dc
DanielvReyes <147454328+DanielvReyes@users.noreply.github.com>
2025-12-03 17:26:32 -0600

    Update generate.go
    This should work better with the html

--- handlers/generate.go
+++ handlers/generate.go
@@ -4,17 +4,39 @@ import (
 	"html/template"
 	"net/http"
 	"password-checker/services"
+	"strconv"
 )
 
 func Generate(tmpl *template.Template, store services.StorageService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		pwd := "example123!" // replace with generator logic
-		store.SavePassword(pwd)
+		var pwd string
+
+		if r.Method == http.MethodPost {
+			// parse form and read length value
+			if err := r.ParseForm(); err == nil {
+				lengthStr := r.FormValue("length")
+				length := 24 // default
+
+				if lengthStr != "" {
+					if n, err := strconv.Atoi(lengthStr); err == nil {
+						//  min max bounds
+						if n < 8 {
+							n = 8
+						} else if n > 32 {
+							n = 32
+						}
+						length = n
+					}
+				}
+
+				pwd = services.GeneratePassword(length)
+				store.SavePassword(pwd)
+			}
+		}
 
 		data := map[string]interface{}{
 			"Password": pwd,
 		}
-		// depends if we use html
 		_ = tmpl.ExecuteTemplate(w, "generate.html", data)
 	}
 }
